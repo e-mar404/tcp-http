8caf0d802dcec0b3eae6c2563b36469af8ca36ae
e-mar404 <egmarinn@gmail.com>
2025-03-25 20:19:00 -0500

    finish ch1

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Print("'I hope I get the job!'")
+	file, err := os.Open("messages.txt")	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	lines := getLinesChannel(file)
+
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesCh := make(chan string)
+
+	go func(){
+		defer close(linesCh)
+
+		currentLine := "" 
+		for {
+			buffer := make([]byte, 8)
+			_, err := f.Read(buffer)
+			if err == io.EOF {
+				return
+			}
+
+			parts := bytes.Split(buffer, []byte("\n"))
+
+			for i := range(len(parts)) {
+				if i != 0 {
+					linesCh <- currentLine
+					currentLine = ""
+				}
+
+				currentLine += string(parts[i])
+			}
+		}
+	}()
+
+	return linesCh
 }
