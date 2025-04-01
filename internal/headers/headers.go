07c266385c514b7db410066c31b9b758674eabe0
e-mar404 <egmarinn@gmail.com>
2025-04-01 18:51:22 -0500

    add code to pass the first couple tests

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,5 +1,12 @@
 package headers
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"unicode"
+)
+
 type Headers map[string]string
 
 func NewHeaders() Headers {
@@ -9,11 +16,28 @@ func NewHeaders() Headers {
 const crlf = "\r\n"
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
-	// get key 
+	bytesRead := 0
+	sepIdx := bytes.Index(data, []byte(":"))
+	if sepIdx == -1 {
+		return 0, false, fmt.Errorf("Malformed header, no `:` separating field key and field value")	
+	}
 
-	// go to the end of the line crlf	
+	if !unicode.IsLetter(rune(data[sepIdx - 1])) {
+		return 0, false, fmt.Errorf("Malformed header, there should be no space between the field key and `:`")
+	}
 
-	// add that to the key
+	fieldName := strings.TrimLeft(string(data[:sepIdx]), " ")
+	bytesRead += len(fieldName)
 
-	// keep going until there are two crlf in a row
+	crlfIdx := bytes.Index(data[sepIdx+1:], []byte(crlf))
+	if crlfIdx == -1 {
+		return 0, false, nil
+	}
+
+	fieldValue := strings.Trim(string(data[sepIdx+1:sepIdx+1+crlfIdx]), " ")
+	bytesRead = bytesRead + len(fieldValue)
+
+	h[fieldName] = fieldValue
+
+	return bytesRead + 4, false, nil 
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -10,11 +10,11 @@ import (
 func TestHeaderParsing(t *testing.T) {
 	// Valid: single header
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n\r\n")
+	data := []byte("Host: localhost:42072\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "localhost:42069", headers["Host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
